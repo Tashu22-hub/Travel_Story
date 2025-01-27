1f466f2c6544674f29bab4ea0c7e332a60d935e0
JAHAN RAMESH <jahanrazh@gmail.com>
2025-01-27 08:57:06 +0530

    add dotenv and mongoose for environment configuration and database connection

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
 
+const mongoose = require('mongoose');
+const config = require('./config.json');
 const bcrypt = require('bcrypt');
 const express = require('express');
 const cors = require('cors');
 
 const jwt = require('jsonwebtoken');
+
+mangoose.connect(config.connectionString); //connect to databass config.json file is used to store the connection string
 
 const app = express();
 app.use(express.json()); 
