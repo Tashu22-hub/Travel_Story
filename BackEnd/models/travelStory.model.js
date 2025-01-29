4bac1e8d56455f52c510b4bc6c57387663a4a97e
JAHAN RAMESH <jahanrazh@gmail.com>
2025-01-29 10:49:16 +0530

    add user reference and additional fields to travelStory schema

--- BackEnd/models/travelStory.model.js
+++ BackEnd/models/travelStory.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");//importing the mongoose package
+const userModel = require("./user.model");
 const Schema = mongoose.Schema;//importing the schema from mongoose
 
 const travelStorySchema = new Schema({
@@ -6,8 +7,11 @@ const travelStorySchema = new Schema({
     content: { type: String, required: true },
     visitedLocations: { type: [String], default: [] },
     isFavaourite: { type: Boolean, default: false },
-    
-    createdOn:{typr:Date,default:Date.now},
+    userId: { type: Schema.Types.ObjectId, ref: "User",required: true },
+    createdOn: { type: Date, default: Date.now },
+    Image: { type: String, required: true },
+    visitedDate: { type: Date, required: true },
 
+});
 
-})
+module.exports = mongoose.model("TravelStory", travelStorySchema);//exporting the user model
