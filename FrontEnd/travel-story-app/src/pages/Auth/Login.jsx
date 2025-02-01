57380cc13f5f49d2460d71244099ce3ab2a8a64e
JAHAN RAMESH <jahanrazh@gmail.com>
2025-02-01 12:13:43 +0530

    feat: enhance login page layout with improved styling and structure

--- FrontEnd/travel-story-app/src/pages/Auth/Login.jsx
+++ FrontEnd/travel-story-app/src/pages/Auth/Login.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      Login
+    <div className='h-screen bg-cyan-50 overflow-hidden relative'>
+      <div className="">
+        <div className="">
+        </div>
+        <div className="">
+          <form onSubmit={() => {}}>
+            <h4 className='text-2xl font-semibold mb-7'>Login</h4>
+
+            <input type="text" placeholder="Email" className='input-box' />
+            
+          </form>
+        </div>
+  
+      </div>
+      
     </div>
   )
 }
