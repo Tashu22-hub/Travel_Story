e2c9d364ab9cc7890044089a6c7254d6c0556c81
JAHAN RAMESH <jahanrazh@gmail.com>
2025-02-01 12:26:01 +0530

    feat: enhance login page with password input and create account button

--- FrontEnd/travel-story-app/src/pages/Auth/Login.jsx
+++ FrontEnd/travel-story-app/src/pages/Auth/Login.jsx
@@ -3,15 +3,29 @@ import React from 'react'
 const Login = () => {
   return (
     <div className='h-screen bg-cyan-50 overflow-hidden relative'>
-      <div className="">
+      <div className="container h-screen flex justify-center items-center px-20 mx-auto">
         <div className="">
         </div>
         <div className="">
           <form onSubmit={() => {}}>
             <h4 className='text-2xl font-semibold mb-7'>Login</h4>
 
-            <input type="text" placeholder="Email" className='input-box' />
-            
+            <input type="text" placeholder="Email" className='input-box' /><br /><br />
+
+            <input type="password" placeholder="Password" className='input-box' /><br/><br />
+
+            <button type="submit" className='btn-primary'>Login</button>
+
+            <p className = "">Or</p>
+
+            <button 
+            type="submit" 
+            className="" 
+            onClick={() => { 
+            navigate ("/signup");
+            }}>
+            CREATE ACCOUNT
+            </button>
           </form>
         </div>
   
