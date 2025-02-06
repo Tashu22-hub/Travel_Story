dc51d5650282b5ae33961bae5c9d0ee003e68f15
JAHAN RAMESH <jahanrazh@gmail.com>
2025-02-06 20:14:10 +0530

    feat: add password visibility toggle to PasswordInput component

--- FrontEnd/travel-story-app/src/components/Input/PasswordInput.jsx
+++ FrontEnd/travel-story-app/src/components/Input/PasswordInput.jsx
@@ -1,17 +1,38 @@
-import React from 'react';
+import React from "react";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
 
 const PasswordInput = ({ value, onChange, placeholder }) => {
+  const [isShowPassword, setIsShowPassword] = React.useState(false);
+
+  const togglePassword = () => {
+    setIsShowPassword(!isShowPassword);
+  };
+
   return (
-    <div className='flex items-center '>
-      <input 
-        type="password" // Set the input type to password
+    <div className="relative w-full mb-4">
+      <input
+        type={isShowPassword ? "text" : "password"}
         value={value}
         onChange={onChange}
         placeholder={placeholder || "Password"}
-        className="w-full p-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-cyan-500"
+        className="w-full p-2 pr-10 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-cyan-500"
       />
+      <div
+        className="absolute inset-y-0 right-2 flex items-center cursor-pointer"
+        onClick={togglePassword}
+      >
+        {isShowPassword ? (
+          <FaRegEye 
+          size={20} 
+          className="text-current cursor-pointer" />
+        ) : (
+          <FaRegEyeSlash 
+          size={20} 
+          className="text-gray-600" />
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default PasswordInput;
