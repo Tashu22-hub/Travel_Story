3181b35c520f00d461bf649a674cf126064c5007
JAHAN RAMESH <jahanrazh@gmail.com>
2025-02-07 17:01:07 +0530

    feat: update Navbar component styling for improved layout and logo size

--- FrontEnd/travel-story-app/src/components/Navbar.jsx
+++ FrontEnd/travel-story-app/src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import LOGO from '../assets/logo.png';
 
 const Navbar = () => {
   return (
-    <div className='bg-white flex items-center justify-between px-2 py-1 drop-shadow sticky top-0 z-10'>
-      <img src={LOGO} alt='logo' className='h-3' />
+    <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow sticky top-0 z-10'>
+      <img src={LOGO} alt='logo' className='h-8' />
     </div>
   );
 };
--- FrontEnd/travel-story-app/src/pages/Home/Home.jsx
+++ FrontEnd/travel-story-app/src/pages/Home/Home.jsx
@@ -1,6 +1,34 @@
 import React from 'react'
 import Navbar from '../../components/Navbar';
+import { use } from 'react';
 const Home = () => {
+
+  const navigate = useNavigate();
+
+  const [userinfo,setUserinfo] = useState(null);
+  
+  //Get user info
+  const getUserInfo = async () => {
+    try {
+      const response = await axiosInstance.get("/get-user");
+      if(response.data && response.data.user){
+        //Set user info in state
+        setUserinfo(response.data.user);
+      }
+    } catch (error) {
+      if (error.response.status === 401) {
+        //clear storage if unauthorized
+        localStorage.clear();
+        navigate("/login");//Redirect to login page
+        setError(error.response.data.message);
+      }
+    }
+  }
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
   return (
     <div>
        <Navbar />
