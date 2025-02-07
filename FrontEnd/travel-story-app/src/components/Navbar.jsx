a33f3a27d0a34770979381a836fbb6bd4f0bd1d5
JAHAN RAMESH <jahanrazh@gmail.com>
2025-02-07 16:53:58 +0530

    feat: add Navbar component to Home page and update Navbar styling

--- FrontEnd/travel-story-app/src/components/Navbar.jsx
+++ FrontEnd/travel-story-app/src/components/Navbar.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-
+import React from 'react';
 import LOGO from '../assets/logo.png';
+
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='bg-white flex items-center justify-between px-2 py-1 drop-shadow sticky top-0 z-10'>
+      <img src={LOGO} alt='logo' className='h-3' />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
