27b1673357351b54dd05e830731ddad57a8da79f
JAHAN RAMESH <jahanrazh@gmail.com>
2025-02-07 16:16:18 +0530

    feat: update SignUp component background image and improve styling; add Navbar component

--- FrontEnd/travel-story-app/src/pages/Auth/SignUp.jsx
+++ FrontEnd/travel-story-app/src/pages/Auth/SignUp.jsx
@@ -76,7 +76,7 @@ const SignUp = () => {
       {/* Main container for signup form */}
       <div className="container h-screen flex justify-center items-center px-20 mx-auto">
         {/* Left Side: Background Image and Text */}
-        <div className="w-2/4 h-[90vh] flex items-end bg-login-bg-img bg-cover bg-center rounded-lg shadow-lg p-10 z-50 relative">
+        <div className="w-2/4 h-[90vh] flex items-end bg-Signup-bg-img bg-cover bg-center rounded-lg shadow-lg p-10 z-50 relative">
           <div className="absolute top-10 left-10 text-white">
             <h4 className="text-4xl font-bold mb-4">
               Capture Your <br /> Journeys
@@ -99,12 +99,12 @@ const SignUp = () => {
               Full Name
             </label>
             <input
-              id="fullName"
+              id="fullName"         // Unique identifier for the input field
               type="text"
               placeholder="Full Name"
               value={name}
-              onChange={({ target }) => {
-                setName(target.value);
+              onChange={({ target }) => {         // Update the name state when the input value changes
+                setName(target.value);       //Update the name state when the input value changes
               }}
               className="w-full p-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-cyan-500"
             />
