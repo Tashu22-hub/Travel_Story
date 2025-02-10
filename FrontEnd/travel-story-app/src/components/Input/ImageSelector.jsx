6259bcd846b19f02915b4e2bb960eccaef95a033
JAHAN RAMESH <jahanrazh@gmail.com>
2025-02-10 13:40:10 +0530

    feat: enhance ImageSelector component; add image upload functionality and preview state management

--- FrontEnd/travel-story-app/src/components/Input/ImageSelector.jsx
+++ FrontEnd/travel-story-app/src/components/Input/ImageSelector.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React,{useRef,useState} from 'react'
+import { FaRegFileImage } from 'react-icons/fa';
+import { MdDeleteOutline} from 'react-icons/md';
 
-const ImageSelector = ({}) => {
+const ImageSelector = ({Image,setImage}) => {
+  const imageRef = useRef(null);
+  
+  const [PreviewUrl, setPreviewUrl] = useState(null);
+
+  const handleImageChange = () => {};
+    
   return (
     <div>
-      ImageSelector
+      <input 
+      type="file" 
+      accept="image/*" 
+      ref={imageRef} 
+      onChange={handleImageChange} 
+      className='hidden' 
+      />
+
+      <button className='' onClick={()=>{}}>
+        <div className=''>
+            <FaRegFileImage className='text-xl text-cyan-500' />
+        </div>
+
+        <p className='text-xs text-slate-500'>Browse image files to upload</p>
+      </button>
+
     </div>
-  )
-}
+  );
+};
 
 export default ImageSelector
