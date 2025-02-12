1aaa90c5252e5b0aaa39bc67d6cde1e8fba102d7
JAHAN RAMESH <jahanrazh@gmail.com>
2025-02-12 12:07:31 +0530

    feat: refactor ViewTravelStory component; update prop names for clarity

--- FrontEnd/travel-story-app/src/pages/Home/ViewTravelStory.jsx
+++ FrontEnd/travel-story-app/src/pages/Home/ViewTravelStory.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import { MdClose, MdDeleteOutline, MdUpdate } from "react-icons/md";
 
-const ViewTravelStory = ({ onClose, onUpdate, onDelete }) => {
+
+const ViewTravelStory = ({onClose,onDeleteClick,onEditClick}) => {
   return (
     <div className='relative'>
       <div className="flex items-center justify-between">
         <div>
           <div className="flex items-center gap-3 bg-cyan-50/50 p-2 rounded-l-lg">
-            <button aria-label="Update Story" className="btn-small" onClick={onUpdate}>
+            <button className="btn-small" onClick={onEditClick}>
               <MdUpdate className="text-lg" />
-              UPDATE STORY
+                    UPDATE STORY
             </button>
-            <button aria-label="Delete" className="btn-small" onClick={onDelete}>
+            <button className="btn-small" onClick={onDeleteClick}>
               <MdDeleteOutline className="text-lg" />
               Delete
             </button>
-            <button aria-label="Close" onClick={onClose}>
+            <button className="" onClick={onClose}>
               <MdClose className="text-lg text-slate-400" />
+             
             </button>
           </div>
         </div>
