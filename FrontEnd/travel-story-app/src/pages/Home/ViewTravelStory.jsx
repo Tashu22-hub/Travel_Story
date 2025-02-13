39b2df9a806e2ac835befe7e6b01ddcd7b2061c6
JAHAN RAMESH <jahanrazh@gmail.com>
2025-02-13 22:15:53 +0530

    feat: remove storyInfo prop from ViewTravelStory component for simplification

--- FrontEnd/travel-story-app/src/pages/Home/ViewTravelStory.jsx
+++ FrontEnd/travel-story-app/src/pages/Home/ViewTravelStory.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { GrMapLocation } from "react-icons/gr";
 import { MdClose, MdDeleteOutline, MdUpdate } from "react-icons/md";
 
 
-const ViewTravelStory = ({storyInfo,onClose,onDeleteClick,onEditClick}) => {
+const ViewTravelStory = ({onClose,onDeleteClick,onEditClick}) => {
   return (
     <div className='relative'>
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-end">
         <div>
           <div className="flex items-center gap-3 bg-cyan-50/50 p-2 rounded-l-lg">
             <button className="btn-small" onClick={onEditClick}>
@@ -23,6 +24,21 @@ const ViewTravelStory = ({storyInfo,onClose,onDeleteClick,onEditClick}) => {
           </div>
         </div>
       </div>
+      <div className="flex-1 flex flex-col gap-2 py-4">
+          <h1 className="text-2xl text-slate-950">
+            {storyInfo && storyInfo.title}
+          </h1>
+          <div className="">
+            <span className="text-sm text-slate-600">
+              {storyInfo && moment(storyInfo.date).format("Do MMM YYYY")}
+            </span>
+            <div className="">
+              <GrMapLocation className="text-sm"/>
+              {storyInfo && storyInfo.visitedLocation.map((item, index) => 
+              storyInfo.visitedLocation.length === index + 1 ? item : `${item},`)}
+            </div>  
+          </div>
+        </div>
     </div>
   );
 };
