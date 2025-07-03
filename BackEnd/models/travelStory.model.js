52ef74a51acaf2d88678d40b65c0ef46beb39bc6
Tashu prajapati <tashu.ug22@nsut.ac.in>
2025-07-03 23:18:45 +0530

    Update travelStory.model.js

--- BackEnd/models/travelStory.model.js
+++ BackEnd/models/travelStory.model.js
@@ -10,7 +10,7 @@ const travelStorySchema = new Schema({
     userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
     createdOn: { type: Date, default: Date.now },
     ImageUrl: { type: String, required: true },
-    visitedDate: { type: Date, required: true },
+    visitedDate: { type: Date, required: true }, 
 });
 
 module.exports = mongoose.model("TravelStory", travelStorySchema); // Exporting the TravelStory model
